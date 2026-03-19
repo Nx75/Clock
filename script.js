2f0667c035c90e88573ea743f5bb467e66358ee1
Nx75 <259840397+Nx75@users.noreply.github.com>
2026-03-19 08:21:19 +0545

    Initalized files and implemented logic to work

---
+++ script.js
@@ -0,0 +1,59 @@
+const btn_12 = document.getElementById("btn-12")
+const btn_24 = document.getElementById("btn-24")
+const time = document.getElementById("time")
+const ampmEl = document.getElementById("abbreviations")
+const date = document.getElementById("date")
+const day = document.getElementById("day")
+
+let hour_12 = true;
+
+function pad(n) {
+    return String(n).padStart(2, "0")
+}
+
+const clock = () => {
+    const now = new Date()
+
+    let h = now.getHours()
+    let m = now.getMinutes()
+    let s = now.getSeconds()
+
+    let suffix = "";
+
+    if (hour_12){
+        suffix = h >= 12 ? "PM" : "AM";
+        h = h % 12 || 12;
+    }
+
+    time.textContent = `${pad(h)}:${pad(m)}:${pad(s)}`
+    ampmEl.textContent = suffix;
+    ampmEl.style.display = hour_12 ? "block" : "none"
+
+    let nowDate = now.toLocaleDateString("en-US",{
+        day : "numeric",
+        month : "long",
+        year: "numeric"
+    })
+
+    let nowDay = now.toLocaleDateString("en-US", {
+        weekday: "long"
+    })
+
+    date.textContent = nowDate;
+    day.textContent = nowDay;
+}
+
+btn_12.addEventListener("click", ()=>{
+    hour_12 = true;
+    btn_12.classList.add("activeToggle");
+    btn_24.classList.remove("activeToggle");
+})
+
+btn_24.addEventListener("click", ()=>{
+    hour_12 = false;
+    btn_24.classList.add("activeToggle");
+    btn_12.classList.remove("activeToggle");
+})
+
+setInterval(clock, 1000)
+clock(); 
